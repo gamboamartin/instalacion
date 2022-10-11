f855360e52ce0e54bca2a61cab7feec3bdaa26b6
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-10-11 14:52:31 -0500

    v0.6.0 se genera crud sucursarl

---
+++ templates/directivas/ins_cuadrilla_html.php
@@ -0,0 +1,30 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\ins_zona;
+use PDO;
+
+class ins_cuadrilla_html extends html_controler {
+    public function select_ins_cuadrilla_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link,
+                                                bool $disabled = false, array $filtro = array(),
+                                                bool $required = false): array|string
+    {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
+        $modelo = new ins_cuadrilla($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, filtro: $filtro,
+            modelo: $modelo, label: 'cuadrilla', name: 'cuadrilla_id', disabled:$disabled, required: $required);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+}
---
+++ templates/inputs/ins_cuadrilla/modifica.php
@@ -0,0 +1,49 @@
+<?php /** @var base\controller\controlador_base $controlador  viene de registros del controler/lista */ ?>
+
+<?php
+use config\generales;
+use config\views;
+use models\gt_autorizante;
+
+?>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="id">Id</label>
+    <div class="controls">
+        <input type="text" name="id" value="<?php echo $controlador->row_upd->id; ?>" class="form-control" required="" id="id" placeholder="Id" readonly>
+    </div>
+</div>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="descripcion">Descripcion</label>
+    <div class="controls">
+        <input type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion; ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
+    </div>
+</div>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="descripcion_select">Descripcion Select</label>
+    <div class="controls">
+        <input type="text" name="descripcion_select" value="<?php echo $controlador->row_upd->descripcion_select; ?>" class="form-control" required="" id="descripcion_select" placeholder="Descripcion Select">
+    </div>
+</div>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="codigo">Codigo</label>
+    <div class="controls">
+        <input type="text" name="codigo" value="<?php echo $controlador->row_upd->codigo; ?>" class="form-control" required="" id="codigo" placeholder="Codigo">
+    </div>
+</div>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="alias">Alias</label>
+    <div class="controls">
+        <input type="text" name="alias" value="<?php echo $controlador->row_upd->alias; ?>" class="form-control" required="" id="alias" placeholder="alias">
+    </div>
+</div>
+<div class="control-group col-sm-6">
+    <label class="control-label" for="codigo_bis">Codigo bis</label>
+    <div class="controls">
+        <input type="text" name="codigo_bis" value="<?php echo $controlador->row_upd->codigo_bis; ?>" class="form-control" required="" id="codigo_bis" placeholder="Codigo bis">
+    </div>
+</div>
+
+<?php echo $controlador->inputs->select->org_sucursal_id; ?>
+
+
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
